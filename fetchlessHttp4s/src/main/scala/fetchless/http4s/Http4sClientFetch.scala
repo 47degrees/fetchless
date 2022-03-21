da69a2ba0ec05accf8d2f51e70905cb66bdd8ad0
Ryan Peters <me@rpeters.dev>
2022-03-21 14:51:56 -0500

    Document http4s client fetches

--- fetchlessHttp4s/src/main/scala/fetchless/http4s/Http4sClientFetch.scala
+++ fetchlessHttp4s/src/main/scala/fetchless/http4s/Http4sClientFetch.scala
@@ -4,13 +4,28 @@ import fetchless.Fetch
 import cats.{Monad, Parallel}
 import org.http4s.{EntityDecoder, Request}
 import org.http4s.client.Client
+import cats.MonadThrow
+import cats.syntax.all._
 
 object Http4sClientFetch {
-  def forEntitySequential[F[_]: Monad, I, A](id: String, client: Client[F])(
+
+  /**
+   * Creates a `Fetch` instance that makes sequential batch queries using `client.expectOption`. Any
+   * HTTP 404/410 responses are resolved as `None` while other HTTP errors will be raised as errors.
+   */
+  def forEntitySequential[F[_]: MonadThrow, I, A](
+      id: String,
+      client: Client[F]
+  )(
       f: I => Request[F]
-  )(implicit ED: EntityDecoder[F, A]) = Fetch.singleSequential[F, I, A](id) { i =>
+  )(implicit ED: EntityDecoder[F, A]) = Fetch.singleSequenced[F, I, A](id) { i =>
     client.expectOption[A](f(i))
   }
+
+  /**
+   * Creates a `Fetch` instance that makes parallel batch queries using `client.expectOption`. Any
+   * HTTP 404/410 responses are resolved as `None` while other HTTP errors will be raised as errors.
+   */
   def forEntityParallel[F[_]: Monad: Parallel, I, A](id: String, client: Client[F])(
       f: I => Request[F]
   )(implicit ED: EntityDecoder[F, A]) =
--- fetchlessHttp4s023/src/main/scala/fetchless/http4s/Http4sClientFetch.scala
+++ fetchlessHttp4s023/src/main/scala/fetchless/http4s/Http4sClientFetch.scala
@@ -8,7 +8,7 @@ import org.http4s.client.Client
 object Http4sClientFetch {
   def forEntitySequential[F[_]: Monad, I, A](id: String, client: Client[F])(
       f: I => Request[F]
-  )(implicit ED: EntityDecoder[F, A]) = Fetch.singleSequential[F, I, A](id) { i =>
+  )(implicit ED: EntityDecoder[F, A]) = Fetch.singleSequenced[F, I, A](id) { i =>
     client.expectOption[A](f(i))
   }
   def forEntityParallel[F[_]: Monad: Parallel, I, A](id: String, client: Client[F])(
