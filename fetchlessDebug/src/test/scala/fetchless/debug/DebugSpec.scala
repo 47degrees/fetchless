61c956fc8e46b38d3f420675b913fd456c9af6e7
Ryan Peters <me@rpeters.dev>
2022-05-10 14:49:27 -0500

    Remove lazy entry from debug log ADT

--- fetchlessDebug/src/test/scala/fetchless/debug/DebugSpec.scala
+++ fetchlessDebug/src/test/scala/fetchless/debug/DebugSpec.scala
@@ -6,6 +6,7 @@ import cats.effect.IO
 import fetchless.syntax._
 import cats.syntax.all._
 import cats.data.Chain
+import cats.Applicative
 
 final class DebugSpec extends CatsEffectSuite {
   test("Debug in sequence") {
@@ -16,7 +17,7 @@ final class DebugSpec extends CatsEffectSuite {
 
       val program = 1.fetch(debugFetch) >>
         2.fetchDedupe(debugFetch) >>
-        3.fetchLazy(debugFetch).run >>
+        3.fetchLazy(Applicative[IO], debugFetch).run >>
         List(4).fetchAll(debugFetch) >>
         List(5).fetchAllDedupe(debugFetch) >>
         List(6).fetchAllLazy(debugFetch).run.void
@@ -24,10 +25,10 @@ final class DebugSpec extends CatsEffectSuite {
       val expectedTypeChain = Chain(
         DebugLog.FetchType.Fetch[Int](1),
         DebugLog.FetchType.FetchDedupe[Int](2),
-        DebugLog.FetchType.FetchLazy[Int](3),
+        DebugLog.FetchType.FetchDedupe[Int](3),
         DebugLog.FetchType.FetchBatch[Int](Set(4)),
         DebugLog.FetchType.FetchBatchDedupe[Int](Set(5)),
-        DebugLog.FetchType.FetchBatchLazy[Int](Set(6))
+        DebugLog.FetchType.FetchBatchDedupe[Int](Set(6))
       )
 
       for {
