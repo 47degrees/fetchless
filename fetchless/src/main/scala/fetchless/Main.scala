45082a4f1754cc3ea15ac74b72b8d817d4bce94c
Ryan Peters <me@rpeters.dev>
2022-05-13 16:05:33 -0500

    Split bench in two (timer vs no timer)

--- fetchless/src/main/scala/fetchless/Main.scala
+++ fetchless/src/main/scala/fetchless/Main.scala
@@ -10,49 +10,62 @@ import scala.concurrent.duration.FiniteDuration
 
 object Main extends IOApp {
   def run(args: List[String]): IO[ExitCode] = {
-    implicit val testFetch: Fetch[IO, Int, Int] =
-      Fetch.singleSequenced[IO, Int, Int]("testFetch")(i => IO.pure(Some(i)))
+    val baseFetch: Fetch[IO, Int, Int] =
+      Fetch.singleSequenced[IO, Int, Int]("testFetch")(i => IO.pure(Some(i))).withTimer
 
-    val initList = (1 to 50000).toList
+    def runTest(implicit testFetch: Fetch[IO, Int, Int]) = {
+      val initList = (1 to 50000).toList
 
-    val testProgram = initList.traverse_(i => testFetch.singleLazy(i)).run
+      val testProgram = initList.traverse_(i => testFetch.singleLazy(i)).run
 
-    val testProgram4 =
-      initList.parTraverse_(i => testFetch.singleLazy(i)).run
+      val testProgram4 =
+        initList.parTraverse_(i => testFetch.singleLazy(i)).run
 
-    val testProgram3 =
-      initList.traverse_(i => testFetch.singleDedupe(i))
+      val testProgram3 =
+        initList.traverse_(i => testFetch.singleDedupe(i))
 
-    val testProgram6 =
-      initList.traverse_(i => testFetch.single(i))
+      val testProgram6 =
+        initList.traverse_(i => testFetch.single(i))
 
-    def collectAvg(f: IO[Unit], n: Int): IO[Double] =
-      IO.ref(Chain.empty[Long]).flatMap { ref =>
-        val addResult = for {
-          start <- Clock[IO].monotonic
-          _     <- f
-          end   <- Clock[IO].monotonic
-          _     <- ref.update(_.append((end - start).length))
-        } yield ()
+      def collectAvg(f: IO[Unit], n: Int): IO[Double] =
+        IO.ref(Chain.empty[Long]).flatMap { ref =>
+          val addResult = for {
+            start <- Clock[IO].monotonic
+            _     <- f
+            end   <- Clock[IO].monotonic
+            _     <- ref.update(_.append((end - start).length))
+          } yield ()
 
-        val getAvg = ref.get.map(_.sumAll.toDouble / n)
+          val getAvg = ref.get.map(_.sumAll.toDouble / n)
 
-        addResult.replicateA_(n) >> getAvg
-      }
+          addResult.replicateA_(n) >> getAvg
+        }
 
-    for {
-      time6 <- collectAvg(testProgram6, 40)
-      time4 <- collectAvg(testProgram4.void, 40)
-      time3 <- collectAvg(testProgram3, 40)
-      time1 <- collectAvg(testProgram.void, 40)
-      _     <- IO.println("Immediate fetch traverse result")
-      _     <- IO.println(time6)
-      _     <- IO.println("Immediate deduped fetch traverse result")
-      _     <- IO.println(time3)
-      _     <- IO.println("LazyRequest traverse result")
-      _     <- IO.println(time1)
-      _     <- IO.println("LazyRequest parTraverse result")
-      _     <- IO.println(time4)
-    } yield ExitCode.Success
+      for {
+        time6 <- collectAvg(testProgram6, 40)
+        time4 <- collectAvg(testProgram4.void, 40)
+        time3 <- collectAvg(testProgram3, 40)
+        time1 <- collectAvg(testProgram.void, 40)
+        _     <- IO.println("Immediate fetch traverse result")
+        _     <- IO.println(time6)
+        _     <- IO.println("Immediate deduped fetch traverse result")
+        _     <- IO.println(time3)
+        _     <- IO.println("LazyRequest traverse result")
+        _     <- IO.println(time1)
+        _     <- IO.println("LazyRequest parTraverse result")
+        _     <- IO.println(time4)
+      } yield ExitCode.Success
+    }
+
+    val printBar = IO.println("- - - - - - - - - -")
+    printBar >>
+      IO.println("Without timer") >>
+      printBar >>
+      runTest(baseFetch) >>
+      IO.println("") >>
+      printBar >>
+      IO.println("With timer") >>
+      printBar >>
+      runTest(baseFetch.withTimer)
   }
 }
