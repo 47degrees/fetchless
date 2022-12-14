3feda4aa81f157f8235baa51531814bbf99a496d
Fede Fernández <720923+fedefernandez@users.noreply.github.com>
2022-12-14 19:04:38 +0100

    Updates cols for Scala 2.12

--- fetchless/src/main/scala-2.12/fetchless/Cols.scala
+++ fetchless/src/main/scala-2.12/fetchless/Cols.scala
@@ -5,8 +5,8 @@ object Cols {
   def map[A, B, C, D](map: Map[A, B])(f: ((A, B)) => (C, D)): Map[C, D] =
     map.map(f)
 
-  def mapValues[A, B, C](map: Map[A, B])(f: B => (C)): Map[A, C] =
-    map.mapValues(f).toMap
+  def mapValues[A, B, C](map: Map[A, B])(f: B => C): Map[A, C] =
+    map.mapValues(f)
 
   def collect[A, B, C, D](map: Map[A, B])(f: PartialFunction[(A, B), (C, D)]): Map[C, D] =
     map.collect(f)
