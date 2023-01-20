9c29dadcb670a7607408d1212c622951099c25d6
fortyseven_degrees <developer@47deg.com>
2023-01-20 05:09:33 +0000

    Update cats-effect to 3.4.5

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -16,7 +16,7 @@ object ProjectPlugin extends AutoPlugin {
       Seq(
         libraryDependencies ++=
           Seq(
-            "org.typelevel" %%% "cats-effect"         % "3.4.4",
+            "org.typelevel" %%% "cats-effect"         % "3.4.5",
             "co.fs2"         %% "fs2-core"            % "3.5.0",
             "org.typelevel" %%% "munit-cats-effect-3" % "1.0.7" % "test"
           )
