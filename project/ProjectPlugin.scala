fea717e7d03ecf58659770e62ea9a750ff170fa7
47erbot <developer@47deg.com>
2023-05-15 07:12:42 +0200

    Update fs2-core to 3.7.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -17,7 +17,7 @@ object ProjectPlugin extends AutoPlugin {
         libraryDependencies ++=
           Seq(
             "org.typelevel" %%% "cats-effect"         % "3.4.11",
-            "co.fs2"         %% "fs2-core"            % "3.6.1",
+            "co.fs2"         %% "fs2-core"            % "3.7.0",
             "org.typelevel" %%% "munit-cats-effect-3" % "1.0.7" % "test"
           )
       )
@@ -26,7 +26,7 @@ object ProjectPlugin extends AutoPlugin {
       Seq(
         libraryDependencies ++=
           Seq(
-            "co.fs2" %% "fs2-core" % "3.6.1"
+            "co.fs2" %% "fs2-core" % "3.7.0"
           )
       )
 
