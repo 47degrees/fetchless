ad059871889747702e15608a028c3fc349d4ce1a
fortyseven_degrees <developer@47deg.com>
2023-06-30 05:07:09 +0000

    Update http4s-client to 0.23.22

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -48,7 +48,7 @@ object ProjectPlugin extends AutoPlugin {
       Seq(
         libraryDependencies ++=
           Seq(
-            "org.http4s" %%% "http4s-client" % "0.23.21"
+            "org.http4s" %%% "http4s-client" % "0.23.22"
           )
       )
 
